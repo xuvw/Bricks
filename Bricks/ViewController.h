daa759c98989b308696f42d86e36197afb3e4417
ke he <smileshitou@hotmail.com>
2017-04-21 14:55:33 +0800

    init

---
+++ Bricks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bricks
+//
+//  Created by heke on 2017/4/21.
+//  Copyright © 2017年 BK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bricks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bricks
+//
+//  Created by heke on 2017/4/21.
+//  Copyright © 2017年 BK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
